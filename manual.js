d2df23065f8ae4adedf0d40eb62a86ad9ee1065f
hararca <hararca@proton.me>
2024-03-30 21:25:09 +0100

    feat: :sparkles: Create manual mode

---
+++ manual.js
@@ -0,0 +1,32 @@
+const iframes = document.querySelectorAll('iframe');
+
+const pages = ['./emotezzz', './kanjizz', './marqueeee', './text-the-void'];
+
+// Initialise the iframes with the pages and display just the first one
+iframes.forEach((iframe, index) => {
+  if (index < pages.length) {
+    iframe.src = pages[index];
+    iframe.style.visibility = 'hidden';
+  }
+  if (index === 0) {
+    iframe.style.visibility = 'visible';
+  }
+});
+
+// Change the visibility of the iframes by selecting the visible with number key
+const changeVisibility = (index) => {
+  iframes.forEach((iframe, i) => {
+    if (i === index) {
+      iframe.style.visibility = 'visible';
+    } else {
+      iframe.style.visibility = 'hidden';
+    }
+  });
+};
+
+// Add event listeners for the number keys
+document.addEventListener('keydown', (event) => {
+  if (event.key >= 1 && event.key <= 4) {
+    changeVisibility(event.key - 1);
+  }
+});
