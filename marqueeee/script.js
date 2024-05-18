68331a412877a7c5448020ea53acc4ce6802e169
hararca <hararca@proton.me>
2024-05-18 13:12:44 +0200

    feat: :sparkles: Add more slogans

--- marqueeee/script.js
+++ marqueeee/script.js
@@ -3,6 +3,15 @@ const slogans = [
   'If You Find This World Bad, You Should See Some of the Others',
   'Dive into the umbilical chaotic zone',
   'The World Wide Web is a broken system, populated with broken computers on broken networks, running broken software written by broken people.',
+  'WHATEVER YOUR NAME IS, GET READY FOR THE BIG SURPRISE!',
+  'OPEN YOUR MIND TO ME',
+  'YOUR WHOLE LIFE IS JUST A DREAM',
+  "IF I'M NOT ME, WHO THE HELL AM I?",
+  'FUCKING MUTANTS!',
+  'NO HAY QUE CONFUNDIR LA LITERATURA CON LA MERMELADA DE FRAMBUESA',
+  'ABRIENDO MENTES. CERRANDO ANTROS',
+  'KEEP YOUR RIÑÓN INSIDE',
+  'Un pequeño paso para el hombre, una puta mierda en general',
 ];
 
 const textElement = document.getElementById('text');
